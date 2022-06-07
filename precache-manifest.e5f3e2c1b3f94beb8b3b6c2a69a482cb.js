1afce8d48b2397d9ec2c977daf93a047f8f13cba
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-06-07 14:21:47 +0200

    Updates

---
+++ precache-manifest.e5f3e2c1b3f94beb8b3b6c2a69a482cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b5b2611f5f536dfec4",
+    "url": "/meet/static/css/main.4f308170.chunk.css"
+  },
+  {
+    "revision": "21b5b2611f5f536dfec4",
+    "url": "/meet/static/js/main.384a9f46.chunk.js"
+  },
+  {
+    "revision": "416d7e159b04be137353",
+    "url": "/meet/static/js/runtime~main.2aebd6ae.js"
+  },
+  {
+    "revision": "86ef4a404ced57ffb431",
+    "url": "/meet/static/js/2.573acf2d.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/meet/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "45e0f72c2731da5794f35bcd6efc2ace",
+    "url": "/meet/index.html"
+  }
+];
