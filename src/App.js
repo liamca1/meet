1dbfad275a14596ede266979201f111caf2c74b1
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-06-14 12:48:15 +0200

    scatter chart added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import WelcomeScreen from './WelcomeScreen';
-
+// import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { OfflineAlert } from './Alert';
 
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { getEvents, extractLocations, checkToken } from './api';
 
 class App extends Component {
 
@@ -53,7 +53,6 @@ class App extends Component {
       });
     }
   }
-  
 
   componentWillUnmount(){
     this.mounted = false;
@@ -83,25 +82,48 @@ class App extends Component {
   })
 }
 
-  render() {
-    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+getData = () => {
+  const {locations, events} = this.state;
+  const data = locations.map((location)=>{
+    const number = events.filter((event) => event.location === location).length
+    const city = location.split(', ').shift()
+    return {city, number};
+  })
+  return data;
+};
 
-    return (
-      <>
-      <div className="topBar">
-        <h2 className="appTitle">Meet App</h2>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents updateEvents={this.updateEvents} />
-      </div>
-      <div className='App'>
-        <OfflineAlert text={this.state.offlineText} />
-        <EventList events={this.state.events} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
-      </div>
-      </>
-      
-    );
-  }
+render() {
+  const { locations, numberOfEvents, events } = this.state;
+  return (
+    <div className="App">
+      <OfflineAlert />
+      <h1>Meet App</h1>
+      <h4>Choose your nearest city</h4>
+      <CitySearch updateEvents={this.updateEvents} locations={locations} />
+      <NumberOfEvents
+        updateEvents={this.updateEvents}
+        numberOfEvents={numberOfEvents}
+      />       
+       <h4>Events in each city</h4>
+
+      <ResponsiveContainer height={400} >
+        <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis
+            allowDecimals={false}
+            type="number"
+            dataKey="number"
+            name="number of events"
+          />
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
+      <EventList events={events} />
+    </div>
+  );
+}
 }
 
 export default App;
